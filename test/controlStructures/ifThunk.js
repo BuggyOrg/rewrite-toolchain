07f5c9bb4e3727c2b6cb92a4138c067adcb71a58
Maximilian Klein <dasmaxl@gmail.com>
2017-03-06 22:45:43 +0100

    (test) Add more tests for ifThunks.

--- test/controlStructures/ifThunk.js
+++ test/controlStructures/ifThunk.js
@@ -21,7 +21,7 @@ const ifNode = (data) =>
 
 describe('» Control Structures', () => {
   describe('» If to ThunkIf', () => {
-    it('can create and compare graphs', () => {
+    it('» create ifThunks for nodes without inputs', () => {
       const graph = Graph.flow(
         Graph.addNode(ifNode({name: 'if'})),
         Graph.addNode({name: 'a', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
@@ -36,6 +36,46 @@ describe('» Control Structures', () => {
       expect(Graph.hasNode('/ifThunk', rewGraph)).to.be.true
       expect(Graph.hasNode('/if', rewGraph)).to.not.be.true
       expect(Graph.nodesBy('/functional/lambda', rewGraph)).to.have.length(2)
+    })
+
+    it('» create ifThunks with multiple inputs', () => {
+      const graph = Graph.flow(
+        Graph.addNode(ifNode({name: 'if'})),
+        Graph.addNode({name: 'a1', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addNode({name: 'a', ports: [{port: 'in', kind: 'input', type: 'Number'}, {port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addNode({name: 'b', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addEdge({from: 'a1@out', to: 'a@in'}),
+        Graph.addEdge({from: 'a@out', to: 'if@a'}),
+        Graph.addEdge({from: 'b@out', to: 'if@b'}),
+        Graph.addEdge({from: '@cond', to: 'if@cond'}),
+        Graph.addEdge({from: 'if@out', to: '@out'})
+      )(Graph.compound({ports: [{port: 'cond', kind: 'input', type: 'Bool'}, {port: 'out', kind: 'output', type: 'generic'}]}))
+
+      const rewGraph = Rewrite.apply(thunkIf, graph)
+      expect(Graph.hasNode('/ifThunk', rewGraph)).to.be.true
+      expect(Graph.hasNode('/if', rewGraph)).to.not.be.true
+      expect(Graph.nodesBy('/functional/lambda', rewGraph)).to.have.length(2)
+    })
+
+    it('» create ifThunks with partials on the inputs', () => {
+      const graph = Graph.flow(
+        Graph.addNode(ifNode({name: 'if'})),
+        Graph.addNode({name: 'a1', ports: [{port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addNode({name: 'a', ports: [{port: 'in', kind: 'input', type: 'Number'}, {port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addNode({name: 'b', ports: [{port: 'in', kind: 'input', type: 'Number'}, {port: 'out', kind: 'output', type: 'Number'}]}),
+        Graph.addEdge({from: 'a1@out', to: 'a@in'}),
+        Graph.addEdge({from: 'a1@out', to: 'b@in'}),
+        Graph.addEdge({from: 'a@out', to: 'if@a'}),
+        Graph.addEdge({from: 'b@out', to: 'if@b'}),
+        Graph.addEdge({from: '@cond', to: 'if@cond'}),
+        Graph.addEdge({from: 'if@out', to: '@out'})
+      )(Graph.compound({ports: [{port: 'cond', kind: 'input', type: 'Bool'}, {port: 'out', kind: 'output', type: 'generic'}]}))
+
+      const rewGraph = Rewrite.apply(thunkIf, graph)
+      expect(Graph.hasNode('/ifThunk', rewGraph)).to.be.true
+      expect(Graph.hasNode('/if', rewGraph)).to.not.be.true
+      expect(Graph.nodesBy('/functional/lambda', rewGraph)).to.have.length(2)
+      expect(Graph.hasNode('a1', graph)).to.be.true
     })
   })
 })
